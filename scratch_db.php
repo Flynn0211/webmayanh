7a2f8eb1d63bec985de9705410f62e387bcefe9a
Flynn <leduongtuananh.nhc@gmail.com>
2026-06-17 09:13:59 +0700

    chore: remove junk test files and zip archives

--- scratch_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'config.php';
-require 'model/database.php';
-$stmt = $conn->query("SELECT ma_hh, ten_hang_hoa, anh FROM hang_hoa WHERE anh LIKE '%img_%'");
-print_r($stmt->fetchAll(PDO::FETCH_ASSOC));
-?>
